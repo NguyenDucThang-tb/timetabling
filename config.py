34c2ee98071b516271785b57ac9d4b11a78ae313
VietNguyen865 <161230559+VietNguyen865@users.noreply.github.com>
2026-04-28 15:40:50 +0700

    Update config.py

--- config.py
+++ config.py
@@ -19,13 +19,13 @@ TOTAL_TIMESLOTS     = 72     # Dataset thực tế có 72 slots
 POP_SIZE        = 100       # Tăng để explore tốt hơn
 GENERATIONS     = 400       # Đủ để hội tụ
 CROSSOVER_PROB  = 0.9
-MUTATION_PROB   = 0.12      # Giảm để tránh phá nghiệm tốt
+MUTATION_PROB   = 0.2      # Giảm để tránh phá nghiệm tốt
 TOURNAMENT_K    = 3
 ELITISM_COUNT   = 4         # Giữ nhiều nghiệm tốt hơn
 
 # Seed từ Greedy (RẤT QUAN TRỌNG)
 USE_GREEDY_SEED = True
-GREEDY_RATIO    = 0.3       # 30% population từ Greedy
+GREEDY_RATIO    = 0.2       # 30% population từ Greedy
 
 
 # ------------------------------------------------------------
@@ -33,7 +33,10 @@ GREEDY_RATIO    = 0.3       # 30% population từ Greedy
 # ------------------------------------------------------------
 
 # Hard constraint phải dominate hoàn toàn
-ALPHA = 1000
+if generation < 100:
+    ALPHA = 100
+else:
+    ALPHA = 1000
 BETA  = 1
 
 # Optional nâng cao (khuyên dùng nếu có thời gian)
@@ -46,9 +49,9 @@ USE_DYNAMIC_ALPHA = True
 
 USE_TABU                = True
 
-LOCAL_SEARCH_ITERATIONS = 400
-NEIGHBOR_SAMPLE_SIZE    = 80
-TABU_TENURE             = 25
+LOCAL_SEARCH_ITERATIONS = 200
+NEIGHBOR_SAMPLE_SIZE    = 50
+TABU_TENURE             = 12
 
 # Chiến lược cực quan trọng
 FOCUS_HARD_FIRST = True   # Ưu tiên sửa hard trước
@@ -58,7 +61,7 @@ FOCUS_HARD_FIRST = True   # Ưu tiên sửa hard trước
 # 4. BACKTRACKING REPAIR
 # ------------------------------------------------------------
 
-MAX_REPAIR_STEPS = 2000
+MAX_REPAIR_STEPS = 800
 MAX_REPAIR_DEPTH = 20
 
 REPAIR_FOCUS_HARD_ONLY = True   # Chỉ sửa hard constraints
@@ -68,7 +71,7 @@ REPAIR_FOCUS_HARD_ONLY = True   # Chỉ sửa hard constraints
 # 5. SOFT CONSTRAINTS (ĐÃ ĐIỀU CHỈNH THEO DATA THẬT)
 # ------------------------------------------------------------
 
-MAX_CONSECUTIVE_SLOTS = 5   # Dataset có ngày dài hơn
+MAX_CONSECUTIVE_SLOTS = 6   # Dataset có ngày dài hơn
 MAX_SLOTS_PER_DAY     = 7
 MAX_GAP_ALLOWED       = 2
 
